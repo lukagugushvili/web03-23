0a5e1954907771fc58a6ac399a73842c1ea290b0
lukagugushvili <gugushvili58@gmail.com>
2023-05-20 04:47:56 -0700

    js

---
+++ script.js
@@ -0,0 +1,26 @@
+// #1
+var firstName = 'luka';
+let lastName = 'gugushvili';
+const age = 19;
+
+// #2 #4
+let y = 7;
+let x = 5;
+
+console.log (y * 2 + x * 2)
+
+// #3
+console.log('i am ' + firstName + ' ' + lastName + ' ' + age + ' ' + 'years old');
+console.log(`i am ${firstName} ${lastName} and i am ${age} years old`);
+
+// #0000
+let car ='cls'
+car = {
+    carName: 'cls',
+    model: '550',
+    age: '2019',
+    horses: '1000',
+};
+
+console.log(`car name is ${car.carName}, model ${car.model}, age ${car.age}, ${car.horses} per horses.`)
+
