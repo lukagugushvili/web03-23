176d700f94dac7f2be1137113d2d120e910e3023
lukagugushvili <gugushvili58@gmail.com>
2023-05-20 17:04:04 -0700

    iPhone-Apple

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const age = 19;
 let y = 7;
 let x = 5;
 
-console.log (y * 2 + x * 2)
+console.log (y * 2 + x * 2);
 
 // #3
 console.log('i am ' + firstName + ' ' + lastName + ' ' + age + ' ' + 'years old');
 console.log(`i am ${firstName} ${lastName} and i am ${age} years old`);
 
 // #0000
-let car ='cls'
+let car ='cls';
 car = {
     carName: 'cls',
     model: '550',
@@ -22,5 +22,5 @@ car = {
     horses: '1000',
 };
 
-console.log(`car name is ${car.carName}, model ${car.model}, age ${car.age}, ${car.horses} per horses.`)
+console.log(`car name is ${car.carName}, model ${car.model}, age ${car.age}, ${car.horses} per horses.`);
 
